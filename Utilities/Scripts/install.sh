7768422dc145e4e6b1a61b4f84d5cdc4d4a8b6d0
szx <szx@null.net>
2018-05-28 00:45:39 +0600

    Update install script

--- Utilities/Scripts/install.sh
+++ Utilities/Scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo cp -r DerivedData/Build/Products/Release/VoodooI2C.kext /Library/Extensions/
 sudo cp -r DerivedData/Build/Products/Release/VoodooI2CELAN.kext /Library/Extensions/
+sudo kextcache -i /
