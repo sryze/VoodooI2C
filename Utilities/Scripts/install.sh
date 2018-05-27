a066bab3e988e7d443fcba27ada6fb786063e16d
szx <szx@null.net>
2018-05-28 00:44:55 +0600

    Add install and uninstall scripts

---
+++ Utilities/Scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo cp -r DerivedData/Build/Products/Release/VoodooI2C.kext /Library/Extensions/
+sudo cp -r DerivedData/Build/Products/Release/VoodooI2CELAN.kext /Library/Extensions/
---
+++ Utilities/Scripts/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo rm -rf /Library/Extensions/VoodooI2C.kext
+sudo rm -rf /Library/Extensions/VoodooI2CELAN.kext
